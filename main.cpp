652d12b4dce5ba5e947c3b6a3a8192ee630cee1c
Marco Calignano <marco.calignano@gmail.com>
2011-10-24 21:08:46 +0200

    adjusted project for cmake build

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "ptedit.h"
 
-#include <QtGui>
-#include <QApplication>
+#include <KDE/KApplication>
+#include <KDE/KAboutData>
+#include <KDE/KCmdLineArgs>
+
+static const char version[] = "0.1";
+
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    KAboutData about("ptedit", 0, ki18n("ptedit"), version, ki18n("pairs theme editor"),
+                     KAboutData::License_GPL, ki18n("(C) 2011 Marco Calignano"), KLocalizedString(), 0, "marco.calignano@gmail.com");
+    KCmdLineArgs::init(argc, argv, &about);
+
+    KApplication app;
     ptedit w;
     w.show();
-    return a.exec();
+    return app.exec();
 }
--- ptedit.pro
+++ ptedit.pro
@@ -1,10 +1,15 @@
 TEMPLATE = app
-TARGET = ptedit 
-
-QT        += core gui xml xmlpatterns 
-
-HEADERS   += ptedit.h
-SOURCES   += main.cpp \
+TARGET = ptedit
+QT += core \
+    gui \
+    xml \
+    xmlpatterns
+HEADERS += ptedit.h
+SOURCES += build/ptedit_automoc.cpp \
+    build/CMakeTmp/check_qt_visibility.cpp \
+    build/CMakeFiles/CompilerIdC/CMakeCCompilerId.c \
+    build/CMakeFiles/CompilerIdCXX/CMakeCXXCompilerId.cpp \
+    main.cpp \
     ptedit.cpp
-FORMS     += ptedit.ui    
-RESOURCES +=
+FORMS += ptedit.ui
+RESOURCES += 
