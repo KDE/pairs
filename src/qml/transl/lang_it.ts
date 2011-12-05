9d293657264bf6097e5f5d1abc1b643622167ae9
Marco Calignano <marco.calignano@gmail.com>
2011-12-05 21:31:46 +0100

    adding Italian translation

---
+++ src/qml/transl/lang_it.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it" sourcelanguage="en">
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="Main.qml" line="48"/>
+        <source>Congratulations!</source>
+        <translation>Congratulazioni</translation>
+    </message>
+    <message>
+        <location filename="Main.qml" line="63"/>
+        <location filename="Main.qml" line="97"/>
+        <source>New Game</source>
+        <translation>Nuovo Gioco</translation>
+    </message>
+    <message>
+        <location filename="Main.qml" line="91"/>
+        <source>Get Themes</source>
+        <translation>Trova Temi</translation>
+    </message>
+    <message>
+        <location filename="Main.qml" line="149"/>
+        <source>Add</source>
+        <translation>Aggiungi</translation>
+    </message>
+</context>
+<context>
+    <name>OptionsPage</name>
+    <message>
+        <location filename="OptionsPage.qml" line="19"/>
+        <source>Pairs</source>
+        <translation>Coppie</translation>
+    </message>
+    <message>
+        <location filename="OptionsPage.qml" line="24"/>
+        <source>Similar</source>
+        <translation>Simili</translation>
+    </message>
+    <message>
+        <location filename="OptionsPage.qml" line="29"/>
+        <source>Logic</source>
+        <translation>Logica</translation>
+    </message>
+    <message>
+        <location filename="OptionsPage.qml" line="34"/>
+        <source>Sound</source>
+        <translation>Suoni</translation>
+    </message>
+    <message>
+        <location filename="OptionsPage.qml" line="39"/>
+        <source>SoundLogic</source>
+        <translation>Suoni e Logica</translation>
+    </message>
+    <message>
+        <location filename="OptionsPage.qml" line="44"/>
+        <source>Words</source>
+        <translation>Parole</translation>
+    </message>
+</context>
+</TS>
