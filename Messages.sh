4573e9e26b8a1c84fc31a616b0a74a8ea5c4b193
Aleix Pol <aleixpol@kde.org>
2012-04-16 21:02:19 +0200

    Remove unused stuff from the Messages.sh file

--- Messages.sh
+++ Messages.sh
@@ -21,15 +21,4 @@
 
 #!/bin/bash
 
-EXTRACTRC=/usr/bin/extractrc
-XGETTEXT=/usr/bin/xgettext
-
- 
-# invoke the extractrc script on all .ui, .rc, and .kcfg files in the sources
-# the results are stored in a pseudo .cpp file to be picked up by xgettext.
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-
-# call xgettext on all source files. If your sources have other filename
-# extensions besides .cc, .cpp, and .h, just add them in the find call.
-$XGETTEXT --keyword=i18n `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/pairs.pot
-rm -f rc.cpp
+$XGETTEXT --keyword=i18n `find . -name \*.cpp -o -name \*.qml` -o $podir/pairs.pot
