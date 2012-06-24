ad33e420122423f24a6e5e96427e22f3a083cd2e
heena mahour <heena393@gmail.com>
2012-06-24 19:57:00 +0530

    added cmake files

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
-#include "ptedit.h"
-
-#include <KDE/KApplication>
-#include <KDE/KAboutData>
-#include <KDE/KCmdLineArgs>
-
-static const char version[] = "0.1";
-
-
-int main(int argc, char *argv[])
+#include"ptedit_1.h"
+#include"kcheckcombobox.h"
+int main(int argc,char *argv[])
 {
-    KAboutData about("ptedit", 0, ki18n("ptedit"), version, ki18n("pairs theme editor"),
-                     KAboutData::License_GPL, ki18n("(C) 2011 Marco Calignano"), KLocalizedString(), 0, "marco.calignano@gmail.com");
-    KCmdLineArgs::init(argc, argv, &about);
-
-    KApplication app;
-    ptedit w;
-    w.show();
-    return app.exec();
+QApplication app(argc,argv);
+QWidget *widget=new QWidget;
+Ui::ptedit_1.ui;
+ui.setupUi(widget);
+widget->show();
+return app.exec();
 }
