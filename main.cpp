0e9e194387d731b986e079ead133ad87578b38a7
heena mahour <heena393@gmail.com>
2012-06-27 16:22:27 +0530

    added running cmake files

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include"ptedit_1.h"
-#include"kcheckcombobox.h"
-int main(int argc,char *argv[])
+#include <QtGui/QApplication>
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
 {
-QApplication app(argc,argv);
-QWidget *widget=new QWidget;
-Ui::ptedit_1.ui;
-ui.setupUi(widget);
-widget->show();
-return app.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    
+    return a.exec();
 }
---
+++ untitled3.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-06-27T03:07:06
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = untitled3
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
