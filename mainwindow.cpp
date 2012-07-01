548643e9dad9b6c6f0c6bcb51817dfc0cca3baef
heena mahour <heena393@gmail.com>
2012-07-01 16:50:22 +0530

    added mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QSplitter.h>
+#include <QSplitter>
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -10,9 +10,14 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {    QSplitter *splitter1 = new QSplitter(parent);
      QSplitter *splitter2 = new QSplitter(parent);
-    splitter1->addWidget(treeview);
-      splitter1->addWidget(widgetgroup);
-      splitter2->addWidget(widgetgroup); splitter2->addWidget(helparea);
+    splitter1->addWidget( ui->treeWidget);
+      splitter1->addWidget(ui->groupBox);
+
+      splitter2->addWidget(ui->groupBox); splitter2->addWidget(ui->scrollArea);
+
+
+
+
     ui->setupUi(this);
 }
 
