ae2dc8f9bb0a42fdcabe89c456b36e26c227c281
heena mahour <heena393@gmail.com>
2012-07-01 16:10:21 +0530

    splitter files

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QSplitter.h>
 
 
 MainWindow::MainWindow(QWidget *parent) :
+
     QMainWindow(parent),
+
     ui(new Ui::MainWindow)
-{
+{    QSplitter *splitter1 = new QSplitter(parent);
+     QSplitter *splitter2 = new QSplitter(parent);
+    splitter1->addWidget(treeview);
+      splitter1->addWidget(widgetgroup);
+      splitter2->addWidget(widgetgroup); splitter2->addWidget(helparea);
     ui->setupUi(this);
 }
 
