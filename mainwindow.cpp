bf1baf0e323860730dc955f6e4eed6160417c4c9
Marco Calignano <marco.calignano@googlemail.com>
2012-09-24 22:38:09 +0200

    new pairs editor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,24 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QSplitter>
+
 
 
 MainWindow::MainWindow(QWidget *parent) :
 
     QMainWindow(parent),
-
     ui(new Ui::MainWindow)
-{    QSplitter *splitter1 = new QSplitter(parent);
-     QSplitter *splitter2 = new QSplitter(parent);
-    splitter1->addWidget( ui->treeWidget);
-      splitter1->addWidget(ui->groupBox);
 
-      splitter2->addWidget(ui->groupBox); splitter2->addWidget(ui->scrollArea);
-
-
-
-
-    ui->setupUi(this);
+{
+	ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
